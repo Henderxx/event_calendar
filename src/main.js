aae0d414fcacdf84caa20f01474410adc6862c3c
Henderxx <henderxx@gmail.com>
2022-01-11 15:03:48 +0100

    working boostrap

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import VCalendar from 'v-calendar';
+
+//import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
 Vue.config.productionTip = true
+Vue.use(VCalendar, {
+  componenPrefix: 'vc',
+})
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
-Vue.use(BootstrapVue)
-Vue.use(IconsPlugin)
+// Vue.use(BootstrapVue)
+// Vue.use(IconsPlugin)
