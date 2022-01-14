c1ff07f5b3f588b56a0c48fdede35a79b7a28839
Henderxx <henderxx@gmail.com>
2022-01-14 15:02:12 +0100

    working Vue cal

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,5 @@
 <template>
   <div id="app">
-        <div class="container col-xxl-8 px-4 py-5">
-          <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
-            <div class="col-10 col-sm-8 col-lg-6">
-              <vue-cal></vue-cal>
-              <!-- <img src="../src/assets/logo.png" class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy"> -->
-            </div>
-            <div class="col-lg-6">
-              <h1 class="display-5 fw-bold lh-1 mb-3">Responsive left-aligned hero with image</h1>
-              <p class="lead">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
-            </div>
-          </div>
-        </div>
-
         <div class="bg-dark text-secondary px-4 py-5 text-center">
             <div class="py-5">
               <h1 class="display-5 fw-bold text-white">Dark mode hero</h1>
@@ -27,16 +14,35 @@
             </div>
           </div>
 
+
+        <div class="container col-xxl-8 px-4 py-5">
+          <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+            <div class="col-10 col-sm-8 col-lg-6">
+              <vue-cal></vue-cal>
+              <!-- <img src="../src/assets/logo.png" class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy"> -->
+            </div>
+            <div class="col-lg-6">
+              <formularz>
+
+              </formularz>
+            </div>
+          </div>
+        </div>
+
+
+
     </div>
 </template>
 
 <script>
 import VueCal from './components/cal2.vue'
+import Form from "./components/formularz.vue"
 
 export default {
   name: 'App',
   components: {
-    'vue-cal': VueCal
+    'vue-cal': VueCal,
+    'formularz': Form,
   }
 }
 </script>
