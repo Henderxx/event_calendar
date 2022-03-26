894ca5ae2f98dd50fe2f87307586e79b454575a2
lukasz-pc <henderxx@gmail.com>
2022-03-26 23:30:02 +0100

    clean

--- src/config/config.js
+++ src/config/config.js
@@ -2,8 +2,8 @@
 module.exports = {
 // axios base path to api
 
-//base_path : 'http://136.243.156.120:32402/api'
-base_path : 'http://136.243.156.120:21155/api'
+base_path : 'https://dannyx123.ct8.pl:32402/api'
+//base_path : 'http://136.243.156.120:21155/api'
 // base_path : 'http://192.168.0.150:32402/api'
 // base_path : 'http://192.168.0.150:21155/api'
  
