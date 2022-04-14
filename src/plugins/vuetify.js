ec1052a9348036013b4b46dbf096375df57c30e1
lukasz-pc <henderxx@gmail.com>
2022-04-14 22:39:58 +0200

    remove preset

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,9 +1,8 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib/framework';
-import { preset } from 'vue-cli-plugin-vuetify-preset-rally/preset'
+import Vue from 'vue'
+import Vuetify from 'vuetify/lib/framework'
 
-Vue.use(Vuetify);
+Vue.use(Vuetify)
 
 export default new Vuetify({
-  preset,
-});
+  theme: { dark: true },
+})
